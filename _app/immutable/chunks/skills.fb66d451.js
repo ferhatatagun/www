bb3373b843d2b45518969bc7707ff58206dfaf8d
ferhatatagun <ferhatatagun2@gmail.com>
2026-06-05 14:17:17 +0000

    Deploying to gh-pages from @ ferhatatagun/www@60c26d88a9bbdf482a65afcc9b6673d2df7a4fbe 🚀

--- _app/immutable/chunks/skills.fb66d451.js
+++ _app/immutable/chunks/skills.fb66d451.js
@@ -1,4 +1,4 @@
-import{A as e}from"./index.6d3ba69a.js";const h=`[![Cybernetically enhanced web apps: Svelte](https://sveltejs.github.io/assets/banner.png)](https://svelte.dev)
+import{A as e}from"./index.5443b0b7.js";const h=`[![Cybernetically enhanced web apps: Svelte](https://sveltejs.github.io/assets/banner.png)](https://svelte.dev)
 
 [![license](https://img.shields.io/npm/l/svelte.svg)](LICENSE.md) [![Chat](https://img.shields.io/discord/457912077277855764?label=chat&logo=discord)](https://svelte.dev/chat)
 
